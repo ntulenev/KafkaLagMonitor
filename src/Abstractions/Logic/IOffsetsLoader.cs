196f2b98218bd05c3cb828c768179586babd69b9
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-28 19:05:59 +0300

    Add IOffsetsLagsLoader

---
+++ src/Abstractions/Logic/IOffsetsLoader.cs
@@ -0,0 +1,11 @@
+﻿using Confluent.Kafka;
+
+using Models;
+
+namespace Abstractions.Logic
+{
+    public interface IOffsetsLagsLoader
+    {
+        public IReadOnlyCollection<PartitionLag> LoadOffsetsLags(IEnumerable<TopicPartition> partitions, TimeSpan timeout);
+    }
+}
