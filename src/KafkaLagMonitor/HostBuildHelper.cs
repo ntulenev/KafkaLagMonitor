07604f7aad07cae46d5ecbb2c0046794383e1d53
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-28 19:38:03 +0300

    Add HostBuildHelper

---
+++ src/KafkaLagMonitor/HostBuildHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace KafkaLagMonitor
+{
+    public static class HostBuildHelper
+    {
+        /// <summary>
+        /// Creates default host for app.
+        /// </summary>
+        public static IHost CreateHost()
+        {
+            var builder = new HostBuilder()
+                   .ConfigureAppConfiguration((hostingContext, config) =>
+                   {
+                       config.RegisterApplicationSettings();
+                   })
+                   .ConfigureServices((hostContext, services) =>
+                   {
+                       services.AddKafka(hostContext);
+                       services.AddAppServices(hostContext);
+                       services.AddLogging(hostContext);
+                   });
+
+            return builder.Build();
+        }
+    }
+}
