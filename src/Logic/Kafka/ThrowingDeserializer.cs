5a2994310551f4ad3bc1566d209b41657ff2e79c
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-28 19:07:11 +0300

    Add ThrowingDeserializer

---
+++ src/Logic/Kafka/ThrowingDeserializer.cs
@@ -0,0 +1,18 @@
+﻿using Confluent.Kafka;
+
+namespace Logic.Kafka
+{
+    public class ThrowingDeserializer : IDeserializer<byte[]>
+    {
+        private static readonly Lazy<ThrowingDeserializer> lazy = new(() => new ThrowingDeserializer());
+
+        public static ThrowingDeserializer Instance => lazy.Value;
+
+        private ThrowingDeserializer() { }
+
+        public byte[] Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
