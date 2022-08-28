0a01c8f7575d199652144183d0a00dd40f771bbc
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-28 19:06:28 +0300

    Add ConsoleTableExporter

---
+++ src/Export/ConsoleTableExporter.cs
@@ -0,0 +1,25 @@
+﻿using Abstractions.Export;
+
+using Models;
+
+using ConsoleTables;
+
+namespace Export
+{
+    internal class ConsoleTableExporter : IExporter
+    {
+        public void Export(IEnumerable<PartitionLag> data, string groupId)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(data));
+            ArgumentNullException.ThrowIfNull(nameof(groupId));
+
+            Console.WriteLine($"GroupId - {groupId}");
+            var table = new ConsoleTable("Topic", "Partition", "Lag");
+            foreach (var item in data)
+            {
+                table.AddRow(item.Topic, item.PartitionId, item.Lag);
+            }
+            table.Write();
+        }
+    }
+}
