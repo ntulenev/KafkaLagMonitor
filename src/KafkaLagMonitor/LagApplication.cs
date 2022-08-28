30cdbdb04d1f6e1c8dbf8a72c7fabda48c285390
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-28 19:37:45 +0300

    Add LagApplication

---
+++ src/KafkaLagMonitor/LagApplication.cs
@@ -0,0 +1,34 @@
+﻿using Abstractions.Export;
+using Abstractions.Logic;
+
+namespace KafkaLagMonitor
+{
+    public class LagApplication
+    {
+        public LagApplication
+            (
+             IOffsetsLagsLoader offsetLoader,
+             ITopicPartitionLoader topicLoader,
+             IExporter exporter
+            )
+        {
+            _offsetLoader = offsetLoader ?? throw new ArgumentNullException(nameof(offsetLoader));
+            _topicLoader = topicLoader ?? throw new ArgumentNullException(nameof(topicLoader));
+            _exporter = exporter ?? throw new ArgumentNullException(nameof(exporter));
+        }
+
+        public void Run()
+        {
+            //TODO Add to options
+            var timeout = TimeSpan.FromSeconds(20);
+
+            var partitions = _topicLoader.LoadPartitions(timeout);
+            var lags = _offsetLoader.LoadOffsetsLags(partitions, timeout);
+            _exporter.Export(lags);
+        }
+
+        private readonly IOffsetsLagsLoader _offsetLoader;
+        private readonly ITopicPartitionLoader _topicLoader;
+        private readonly IExporter _exporter;
+    }
+}
