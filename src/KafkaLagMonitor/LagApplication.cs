190821215203a0f704bf143d4a1830a2796e0fc2
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-30 19:17:43 +0300

    Add LagApplicationConfiguration in LagApplication

--- src/KafkaLagMonitor/LagApplication.cs
+++ src/KafkaLagMonitor/LagApplication.cs
@@ -1,30 +1,40 @@
 ﻿using Abstractions.Export;
 using Abstractions.Logic;
+using KafkaLagMonitor.Configuration;
+using Microsoft.Extensions.Options;
 using Models;
 
 namespace KafkaLagMonitor
 {
     public class LagApplication
     {
-        public LagApplication(ILagLoader loader,
+        public LagApplication(IOptions<LagApplicationConfiguration> options,
+                              ILagLoader loader,
                               IExporter exporter
                               )
         {
+            ArgumentNullException.ThrowIfNull(options);
+
             _loader = loader ?? throw new ArgumentNullException(nameof(loader));
             _exporter = exporter ?? throw new ArgumentNullException(nameof(exporter));
+            _options = options.Value ?? throw new ArgumentException("Options value is not set", nameof(options));
         }
 
         public void Run()
         {
-            //TODO Add to options
-            var timeout = TimeSpan.FromSeconds(20);
-            var group = new GroupId("commands");
+            foreach (var groupName in _options.Groups)
+            {
+                var group = new GroupId(groupName);
 
-            var lags = _loader.LoadOffsetsLags(group, timeout);
-            _exporter.Export(lags);
+                Console.WriteLine(group.Value);
+
+                var lags = _loader.LoadOffsetsLags(group, _options.Timeout);
+                _exporter.Export(lags);
+            }
         }
 
         private readonly ILagLoader _loader;
         private readonly IExporter _exporter;
+        private readonly LagApplicationConfiguration _options;
     }
 }
