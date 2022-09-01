a49e408a9798e2956eb07e6bc9d853c5f3e94419
Nikita Tulenev <n1tulenev@gmail.com>
2022-09-01 18:30:56 +0300

    Add BootstrapServersConfiguration

---
+++ src/KafkaLagMonitor/Configuration/BootstrapServersConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaLagMonitor.Configuration
+{
+    /// <summary>
+    /// Bootstrap servers configuration.
+    /// </summary>
+    public class BootstrapServersConfiguration
+    {
+        /// <summary>
+        /// List of bootstrap servers.
+        /// </summary>
+        public List<string> BootstrapServers { get; set; } = default!;
+
+        /// <summary>
+        /// Kafka user name, is any.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Kafka password name, is any.
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Kafka security protocol.
+        /// </summary>
+        public SecurityProtocol SecurityProtocol { get; set; } = SecurityProtocol.Plaintext;
+
+        /// <summary>
+        /// Kafka security protocol mechanism.
+        /// </summary>
+        public SaslMechanism? SASLMechanism { get; set; }
+    }
+}
