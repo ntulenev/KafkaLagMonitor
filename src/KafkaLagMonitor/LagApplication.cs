9dfc42f35fb90ad9e4e7fa0ce375b2f7a8aebebd
Nikita Tulenev <n1tulenev@gmail.com>
2022-10-22 08:41:59 +0300

    Refactor LagApplication

--- src/KafkaLagMonitor/LagApplication.cs
+++ src/KafkaLagMonitor/LagApplication.cs
@@ -29,7 +29,14 @@ namespace KafkaLagMonitor
 
             _loader = loader ?? throw new ArgumentNullException(nameof(loader));
             _exporter = exporter ?? throw new ArgumentNullException(nameof(exporter));
-            _options = options.Value ?? throw new ArgumentException("Options value is not set", nameof(options));
+
+            if (options.Value is null)
+            {
+                throw new ArgumentException("Options value is not set", nameof(options));
+            }
+
+            _timeout = options.Value.Timeout;
+            _groups = options.Value.GetGroups();
         }
 
         /// <summary>
@@ -37,19 +44,18 @@ namespace KafkaLagMonitor
         /// </summary>
         public void Run()
         {
-            foreach (var groupName in _options.Groups)
+            foreach (var group in _groups)
             {
-                var group = new GroupId(groupName);
-
                 Console.WriteLine(group.Value);
 
-                var lags = _loader.LoadOffsetsLags(group, _options.Timeout);
+                var lags = _loader.LoadOffsetsLags(group, _timeout);
                 _exporter.Export(lags);
             }
         }
 
         private readonly ILagLoader _loader;
         private readonly IExporter _exporter;
-        private readonly LagApplicationConfiguration _options;
+        private readonly IEnumerable<GroupId> _groups;
+        private readonly TimeSpan _timeout;
     }
 }
