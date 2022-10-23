06785764d6bab69c72696508eff4b72c5dd25695
Nikita Tulenev <n1tulenev@gmail.com>
2022-10-23 09:36:02 +0300

    Add GroupId tests

---
+++ src/Models.Tests/GroupId.Tests.cs
@@ -0,0 +1,45 @@
+namespace Models.Tests
+{
+    public class GroupIdTests
+    {
+        [Fact]
+        public void GroupIdCanBeCreated()
+        {
+            // Arrange
+            var name = "test";
+
+            // Act
+            var groupId = new GroupId(name);
+
+            // Assert
+            groupId.Value.Should().Be(name);
+        }
+
+        [Theory]
+        [InlineData((string)null!)]
+        [InlineData("")]
+        public void GroupIdCantBeCreatedWithNullOrEmpty(string data)
+        {
+            // Arrange
+
+            // Act
+            var exception = Record.Exception(() => new GroupId(data));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GroupIdCantBeCreatedWithNameOfSpaces()
+        {
+            // Arrange
+            var name = "     ";
+
+            // Act
+            var exception = Record.Exception(() => new GroupId(name));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+    }
+}
