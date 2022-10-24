85ca4a6fd8afbcb08e69c14e5ff2a27be41bf441
Nikita Tulenev <n1tulenev@gmail.com>
2022-10-24 18:50:26 +0300

    Add PartitionLag tests

---
+++ src/Models.Tests/PartitionLag.Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Tests
+{
+    public class PartitionLagTests
+    {
+        [Fact]
+        public void PartitionLagCanBeCreatedWithSpecialOffset()
+        {
+            // Arrange
+            var topicName = "123";
+            var partitionId = 42;
+            var partition = new Confluent.Kafka.TopicPartition(topicName, new Confluent.Kafka.Partition(partitionId));
+            var offset = new Confluent.Kafka.TopicPartitionOffset(partition, Confluent.Kafka.Offset.Beginning);
+
+            // Act
+            var lag = new PartitionLag(offset);
+
+            // Assert
+            lag.Topic.Should().Be(topicName);
+            lag.PartitionId.Should().Be(partitionId);
+            lag.Lag.Should().Be(Confluent.Kafka.Offset.Beginning);
+        }
+
+        [Fact]
+        public void PartitionLagCantBeCreatedWithNonSpecialOffsetAndNoWatermark()
+        {
+            // Arrange
+            var topicName = "123";
+            var partitionId = 42;
+            var partition = new Confluent.Kafka.TopicPartition(topicName, new Confluent.Kafka.Partition(partitionId));
+            var offset = new Confluent.Kafka.TopicPartitionOffset(partition, new Confluent.Kafka.Offset(1));
+
+            // Act
+            var exception = Record.Exception(() => new PartitionLag(offset));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<InvalidOperationException>();
+        }
+    }
+}
