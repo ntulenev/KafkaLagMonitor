110104b1fbe136bc3e681a52a3cf4e9c5e7d7458
Nikita Tulenev <n1tulenev@gmail.com>
2022-10-27 18:38:20 +0300

    Add GroupLagResult Tests

---
+++ src/Models.Tests/GroupLagResult.Tests.cs
@@ -0,0 +1,46 @@
+﻿namespace Models.Tests
+{
+    public class GroupLagResultTests
+    {
+        [Fact]
+        public void GroupLagResultCanBeCreated()
+        {
+            // Arrange
+            var groupId = new GroupId("1");
+            var lags = Enumerable.Empty<PartitionLag>();
+
+            // Act
+            var result = new GroupLagResult(groupId, lags);
+
+            // Assert
+            result.Group.Should().Be(groupId);
+            (result.Lags == lags).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GroupLagResultCantBeCreatedWithNullGroupId()
+        {
+            // Arrange
+            var lags = Enumerable.Empty<PartitionLag>();
+
+            // Act
+            var exception = Record.Exception(() => new GroupLagResult(null!, lags));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GroupLagResultCantBeCreatedWithNullLags()
+        {
+            // Arrange
+            var groupId = new GroupId("1");
+
+            // Act
+            var exception = Record.Exception(() => new GroupLagResult(groupId, null!));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+    }
+}
