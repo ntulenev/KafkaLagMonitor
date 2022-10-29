b78ea8d82cd10f2334cb2089c824433c2461506a
Nikita Tulenev <n1tulenev@gmail.com>
2022-10-29 14:48:31 +0300

    Add ConsoleTableExporter Tests

---
+++ src/Export.Tests/ConsoleTableExporter.Tests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+
+namespace Export.Tests
+{
+    public class ConsoleTableExporterTests
+    {
+        [Fact]
+        public void ConsoleTableExporterCanBeCreated()
+        {
+            // Arrange
+
+            // Act
+            var exception = Record.Exception(() => new ConsoleTableExporter());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact]
+        public void ConsoleTableExporterCanExportData()
+        {
+            // Arrange
+            var exporter = new ConsoleTableExporter();
+            var exportModel = new Models.GroupLagResult(new Models.GroupId("1"), new[]
+            {
+                new Models.PartitionLag(new Confluent.Kafka.TopicPartitionOffset(
+                    new Confluent.Kafka.TopicPartition("a",new Confluent.Kafka.Partition(1)),Confluent.Kafka.Offset.Beginning))
+            });
+
+            // Act
+            var exception = Record.Exception(() => exporter.Export(exportModel));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact]
+        public void ConsoleTableExporterCantExportNullData()
+        {
+            // Arrange
+            var exporter = new ConsoleTableExporter();
+
+            // Act
+            var exception = Record.Exception(() => exporter.Export(null!));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+    }
+}
