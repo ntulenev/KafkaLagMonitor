07ab1d68764d57beb7777264ea203763f232f9af
Nikita Tulenev <n1tulenev@gmail.com>
2022-11-02 20:16:42 +0300

    Add LoadOffsetsLags Tests

--- src/Logic.Tests/LagLoader.Tests.cs
+++ src/Logic.Tests/LagLoader.Tests.cs
@@ -1,4 +1,6 @@
 ﻿using Abstractions.Logic;
+using Confluent.Kafka;
+using Models;
 
 namespace Logic.Tests;
 
@@ -42,5 +44,56 @@ public class LagLoaderTests
 
         // Assert
         exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void LadLoaderCantLoadForNullGroupId()
+    {
+        // Arrange
+        var offsetLoader = Mock.Of<IOffsetsLagsLoader>(MockBehavior.Strict);
+        var topicLoader = Mock.Of<ITopicPartitionLoader>(MockBehavior.Strict);
+        var loader = new LagLoader(offsetLoader, topicLoader);
+        var timeSpan = TimeSpan.FromSeconds(1);
+
+        // Act
+        var exception = Record.Exception(() => loader.LoadOffsetsLags(null!, timeSpan));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void LadLoaderCanLoadData()
+    {
+        // Arrange
+        var timeSpan = TimeSpan.FromSeconds(1);
+        var group = new GroupId("data");
+        var topOffset = 20;
+        var lowOffset = 5;
+        var topicName = "123";
+        var partitionId = 42;
+        var partition = new Confluent.Kafka.TopicPartition(topicName, new Confluent.Kafka.Partition(partitionId));
+        var offset = new Confluent.Kafka.TopicPartitionOffset(partition, lowOffset);
+        var wm = new Confluent.Kafka.WatermarkOffsets(new Confluent.Kafka.Offset(1), new Confluent.Kafka.Offset(topOffset));
+        var lags = new[]
+        {
+            new PartitionLag(offset, wm)
+        };
+        var expectedResult = new GroupLagResult(group, lags);
+        var partitions = new[]
+        {
+            new TopicPartition("a",new Partition(1))
+        };
+        var topicLoader = new Mock<ITopicPartitionLoader>(MockBehavior.Strict);
+        topicLoader.Setup(x => x.LoadPartitions(timeSpan)).Returns(partitions);
+        var offsetLoader = new Mock<IOffsetsLagsLoader>(MockBehavior.Strict);
+        offsetLoader.Setup(x => x.LoadOffsetsLags(partitions, group, timeSpan)).Returns(expectedResult);
+        var loader = new LagLoader(offsetLoader.Object, topicLoader.Object);
+
+        // Act
+        var result = loader.LoadOffsetsLags(group, timeSpan);
+
+        // Assert
+        result.Should().Be(expectedResult);
     }
 }
