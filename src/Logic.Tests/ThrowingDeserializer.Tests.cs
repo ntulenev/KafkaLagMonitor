c42375ad157c05193e33a501d40e312ec293582f
Nikita Tulenev <n1tulenev@gmail.com>
2022-11-06 16:48:53 +0100

    Add ThrowingDeserializer tests

---
+++ src/Logic.Tests/ThrowingDeserializer.Tests.cs
@@ -0,0 +1,16 @@
+﻿using Logic.Kafka;
+
+namespace Logic.Tests;
+
+public class ThrowingDeserializerTests
+{
+    [Fact]
+    public void ThrowingDeserializerCanBeCreated()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = ThrowingDeserializer.Instance);
+
+        // Assert
+        exception.Should().BeNull();
+    }
+}
