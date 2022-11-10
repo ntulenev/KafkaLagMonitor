6821dedd5c8d34c255093a817ed9937557ab4e68
Nikita Tulenev <n1tulenev@gmail.com>
2022-11-10 17:21:23 +0100

    Add BootstrapServersConfiguration Tests

---
+++ src/KafkaLagMonitor.Tests/BootstrapServersConfiguration.Tests.cs
@@ -0,0 +1,22 @@
+﻿using KafkaLagMonitor.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaLagMonitor.Tests
+{
+    public class BootstrapServersConfigurationTests
+    {
+        [Fact]
+        public void BootstrapServersConfigurationCanBeCreated()
+        {
+            // Act
+            var exception = Record.Exception(() => new BootstrapServersConfiguration());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
