1e761d0aa105b03ca7cd437b4062e5e310acdfd7
Nikita Tulenev <n1tulenev@gmail.com>
2022-11-10 17:21:11 +0100

    Add LagApplicationConfiguration Tests

---
+++ src/KafkaLagMonitor.Tests/LagApplicationConfiguration.Tests.cs
@@ -0,0 +1,44 @@
+﻿using KafkaLagMonitor.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaLagMonitor.Tests
+{
+    public class LagApplicationConfigurationTests
+    {
+        [Fact]
+        public void LagApplicationConfigurationCanBeCreated()
+        {
+            // Act
+            var exception = Record.Exception(() => new LagApplicationConfiguration());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact]
+        public void LagApplicationConfigurationReturnsCorrectGroups()
+        {
+            // Arrange
+            var config = new LagApplicationConfiguration()
+            {
+                Groups = new List<string>()
+                  {
+                      "A","B"
+                  }
+            };
+
+            // Act
+            var groups = config.GetGroups();
+
+            // Assert
+            groups.Should().HaveCount(2);
+            var items = groups.ToArray();
+            items[0].Value.Should().Be("A");
+            items[1].Value.Should().Be("B");
+        }
+    }
+}
