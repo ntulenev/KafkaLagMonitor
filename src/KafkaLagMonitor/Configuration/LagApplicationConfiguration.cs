52f0bf0f5a412663bac4ebd092b325bd09d1c96b
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-02 09:44:57 +0100

    Improves LagApplicationConfiguration immutability
    Ensures immutability by using init properties and updating the GetGroups method.
    Suppresses CA1515 and CA1721 warnings to avoid unnecessary recommendations.

--- src/KafkaLagMonitor/Configuration/LagApplicationConfiguration.cs
+++ src/KafkaLagMonitor/Configuration/LagApplicationConfiguration.cs
@@ -1,24 +1,30 @@
-﻿using Models;
+using Models;
 
 namespace KafkaLagMonitor.Configuration;
 
 /// <summary>
 /// Lag application configuration.
 /// </summary>
+#pragma warning disable CA1515 // Consider making public types internal
 public class LagApplicationConfiguration
+#pragma warning restore CA1515 // Consider making public types internal
 {
     /// <summary>
     /// List of groups where we need to check the offsets.
     /// </summary>
-    public List<string> Groups { get; set; } = null!;
+#pragma warning disable CA1721 // Property names should not match get methods
+#pragma warning disable CA1002 // Do not expose generic lists
+    public List<string> Groups { get; init; } = null!;
+#pragma warning restore CA1002 // Do not expose generic lists
+#pragma warning restore CA1721 // Property names should not match get methods
 
     /// <summary>
     /// Offsets request timeout.
     /// </summary>
-    public TimeSpan Timeout { get; set; }
+    public TimeSpan Timeout { get; init; }
 
     /// <summary>
     /// Gets Group Ids.
     /// </summary>
-    public IReadOnlyCollection<GroupId> GetGroups() => Groups.Select(x => new GroupId(x)).ToList();
+    public IReadOnlyCollection<GroupId> GetGroups() => [.. Groups.Select(x => new GroupId(x))];
 }
