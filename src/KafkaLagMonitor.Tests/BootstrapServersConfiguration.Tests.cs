73096e4debca1528748e47a8b5b56420ab37a060
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-02 09:45:08 +0100

    Updates BootstrapServersConfigurationTests class
    Updates the `BootstrapServersConfigurationTests` class to use the new namespace declaration syntax.

--- src/KafkaLagMonitor.Tests/BootstrapServersConfiguration.Tests.cs
+++ src/KafkaLagMonitor.Tests/BootstrapServersConfiguration.Tests.cs
@@ -1,17 +1,16 @@
-﻿using KafkaLagMonitor.Configuration;
+using KafkaLagMonitor.Configuration;
 
-namespace KafkaLagMonitor.Tests
+namespace KafkaLagMonitor.Tests;
+
+public class BootstrapServersConfigurationTests
 {
-    public class BootstrapServersConfigurationTests
+    [Fact]
+    public void BootstrapServersConfigurationCanBeCreated()
     {
-        [Fact]
-        public void BootstrapServersConfigurationCanBeCreated()
-        {
-            // Act
-            var exception = Record.Exception(() => new BootstrapServersConfiguration());
+        // Act
+        var exception = Record.Exception(() => new BootstrapServersConfiguration());
 
-            // Assert
-            exception.Should().BeNull();
-        }
+        // Assert
+        exception.Should().BeNull();
     }
 }
