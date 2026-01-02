dbd41182670a9b757828b56ac613bfb47a6ab673
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-02 09:44:31 +0100

    Adds global suppressions for code analysis
    Adds a GlobalSuppressions.cs file to define project-level code analysis suppressions.
    
    This file contains attributes to suppress specific code analysis warnings, such as IDE0058 (Expression value is never used) and IDE0046 (Convert to conditional expression), in certain members of the KafkaLagMonitor project.
    
    The suppressions are added to avoid unnecessary warnings and improve code clarity, while further investigation is pending.

---
+++ src/KafkaLagMonitor/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.StartupHelper.RegisterConfigs(Microsoft.Extensions.DependencyInjection.IServiceCollection,Microsoft.Extensions.Configuration.IConfiguration)")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.StartupHelper.RegisterApplicationSettings(Microsoft.Extensions.Configuration.IConfigurationBuilder)")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.StartupHelper.AddLogging(Microsoft.Extensions.DependencyInjection.IServiceCollection,Microsoft.Extensions.Configuration.IConfiguration)")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.StartupHelper.AddAppServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.StartupHelper.AddKafka(Microsoft.Extensions.DependencyInjection.IServiceCollection,Microsoft.Extensions.Configuration.IConfiguration)")]
+[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.Configuration.Validation.LagApplicationConfigurationValidator.Validate(System.String,KafkaLagMonitor.Configuration.LagApplicationConfiguration)~Microsoft.Extensions.Options.ValidateOptionsResult")]
+[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:KafkaLagMonitor.Configuration.Validation.BootstrapServersConfigurationValidator.Validate(System.String,KafkaLagMonitor.Configuration.BootstrapServersConfiguration)~Microsoft.Extensions.Options.ValidateOptionsResult")]
