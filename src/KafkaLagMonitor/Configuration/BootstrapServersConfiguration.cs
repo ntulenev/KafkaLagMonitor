bbacaa188d6690d123350f4d17d34b735601f84e
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-02 09:45:03 +0100

    Updates bootstrap server configuration
    Changes the properties of the bootstrap server configuration to use init;
    This ensures that these properties can only be set during object initialization, promoting immutability.
    Also suppresses some warnings related to code analysis.

--- src/KafkaLagMonitor/Configuration/BootstrapServersConfiguration.cs
+++ src/KafkaLagMonitor/Configuration/BootstrapServersConfiguration.cs
@@ -1,34 +1,38 @@
-﻿using Confluent.Kafka;
+using Confluent.Kafka;
 
 namespace KafkaLagMonitor.Configuration;
 
 /// <summary>
 /// Bootstrap servers configuration.
 /// </summary>
+#pragma warning disable CA1515 // Consider making public types internal
 public class BootstrapServersConfiguration
+#pragma warning restore CA1515 // Consider making public types internal
 {
     /// <summary>
     /// List of bootstrap servers.
     /// </summary>
-    public List<string> BootstrapServers { get; set; } = default!;
+#pragma warning disable CA1002 // Do not expose generic lists
+    public List<string> BootstrapServers { get; init; } = default!;
+#pragma warning restore CA1002 // Do not expose generic lists
 
     /// <summary>
     /// Kafka user name, is any.
     /// </summary>
-    public string? Username { get; set; }
+    public string? Username { get; init; }
 
     /// <summary>
     /// Kafka password name, is any.
     /// </summary>
-    public string? Password { get; set; }
+    public string? Password { get; init; }
 
     /// <summary>
     /// Kafka security protocol.
     /// </summary>
-    public SecurityProtocol SecurityProtocol { get; set; } = SecurityProtocol.Plaintext;
+    public SecurityProtocol SecurityProtocol { get; init; } = SecurityProtocol.Plaintext;
 
     /// <summary>
     /// Kafka security protocol mechanism.
     /// </summary>
-    public SaslMechanism? SASLMechanism { get; set; }
+    public SaslMechanism? SASLMechanism { get; init; }
 }
